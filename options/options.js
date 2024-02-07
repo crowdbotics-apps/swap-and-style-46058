f0e581225053245e8bb62a87a26414c284911e3f
Crowdbotics <team@crowdbotics.com>
2024-02-07 15:39:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "swap-and-style-46058",
+  url: "https://swap-and-style-46058.botics.co",
+  api: "https://swap-and-style-46058.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
